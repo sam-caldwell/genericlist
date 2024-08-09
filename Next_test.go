9d043e4e4a6b5b27f78629304a4d9f353f9918e3
Sam Caldwell <github@samcaldwell.net>
2024-08-09 11:15:36 -0500

    add Prev() and Next() tests

---
+++ Next_test.go
@@ -0,0 +1,23 @@
+package genericlist
+
+import "testing"
+
+func TestLinkedList_Next(t *testing.T) {
+	list := &LinkedList[int]{}
+	for i := 1; i <= 10; i++ {
+		list.AddTail(i)
+		if list.count != list.Count() {
+			t.Fatalf("List[%d] = %d; want %d", i, list.count, list.Count())
+		}
+		if list.count != uint(i) {
+			t.Fatalf("List[%d] = %d; want %d", i, list.count, i)
+		}
+	}
+	list.curr = list.head
+	for i := 1; i <= 10; i++ {
+		if list.curr.data != i {
+			t.Fatalf("got %d, want %d", list.curr.data, i)
+		}
+		list.Next()
+	}
+}
