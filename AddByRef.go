061b438fb95307636c3ae457756825d53761186b
Sam Caldwell <github@samcaldwell.net>
2024-08-09 18:28:33 -0500

    add AddByRef() to accept data objects by reference

---
+++ AddByRef.go
@@ -0,0 +1,27 @@
+package genericlist
+
+// AddByRef - Add a node by reference.
+func (list *LinkedList[T]) AddByRef(data *T) {
+	list.lock.Lock()
+	defer list.lock.Unlock()
+
+	newNode := &LinkedListNode[T]{data: *data}
+
+	if list.curr == nil { // List is empty
+		list.head = newNode
+		list.tail = newNode
+		list.curr = newNode
+	} else {
+		newNode.nextPtr = list.curr.nextPtr
+		if list.curr.nextPtr != nil {
+			list.curr.nextPtr.prevPtr = newNode
+		} else {
+			list.tail = newNode // Update tail if the new node is the last one
+		}
+		list.curr.nextPtr = newNode
+		newNode.prevPtr = list.curr
+		list.curr = newNode // Move curr to the new node
+	}
+	list.count++ // Increment count
+	list.sorted = false
+}
